c2ab75c6eaa1a879d4b1befbb41e46dcbb2bb0e9
Anisa Muziya Rafa <amrxll08@gmail.com>
2024-10-22 07:17:52 +0800

    Update list.py

--- list.py
+++ list.py
@@ -1,8 +1,8 @@
 buah_buahan = ["naga","rambutan", "mangga", "jambu"]
-panjang_my_list = len(buah_buahan)
+panjang_buah_buahan = len(buah_buahan)
 
 print("buah-buahan: ", buah_buahan)
-print("panjang list: ", panjang_my_list)
+print("panjang list: ", panjang_buah_buahan)
 print("panggil index ke 3: ", buah_buahan[3])
 
 buah_buahan.append("pisang")
@@ -12,5 +12,4 @@ buah_buahan.remove("mangga")
 print("buah-buahan setelah menghapus nilai mangga: ", buah_buahan)
 
 del buah_buahan[1]
-
 print("buah_buahan setelah menghapus index ke 1: ", buah_buahan)
